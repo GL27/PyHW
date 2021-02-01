e41df69743dda6cf5e3002f3793b30a02ccb4889
GL27 <gabyliang2001@gmail.com>
2021-02-01 13:16:44 -0500

    Testing for Q1

--- Homework/HW7.py
+++ Homework/HW7.py
@@ -38,9 +38,9 @@ def flip_phone(message):
     }
     result = ''
     for character in message:
-        for x in dict.items():
-            if character in x:
-                for each in range(x.index(x) + 1):
+        for x, y in dict.items():
+            if character in y:
+                for each in range(y.index(x) + 1):
                     result += x
     return result
 
